0f711ec9938a351f3727108ebc111e8db4c4fc44
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-07-30 08:16:32 -0400

    Initial commit

---
+++ MarkUpChecker.js
@@ -0,0 +1,25 @@
+/* My original stab at the markup checker */
+var AvonURLChecker = (function iffe() {
+  var publicAPI = {
+        getAvonURL: function() {
+            for (var i = 0; i < arguments.length; i++) {
+                return {
+                    'smo': 'http://smo.rye.avon.com',
+                    'smodev': 'http://smodev.rye.avon.com',
+                    'youravon_sans_w': 'http://youravon.com',
+                    'youravon': 'http://www.youravon.com',
+                    'youravon2': 'http://www2.youravon.com'
+                }[arguments[i]];
+            }
+        },
+        searchAvonURL: function(url) {
+            for (var i = 0, l = document.links.length; i < l; i++) {
+                if (document.links[i].href.indexOf(publicAPI.getAvonURL(url) !== -1)) {
+                    find document.links[i].setAttribute("target", "_blank");
+                }
+            }
+        }
+    };
+
+    return publicAPI;
+})();
